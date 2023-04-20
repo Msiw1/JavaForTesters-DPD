feaf3c32d6cbaa82d18281cae788141026513bdc
Msiw1 <mateusz.siwicki@dpdgroup.com>
2023-04-20 18:21:07 +0200

    Task 1,2,3,4

--- src/main/java/a_introduction/Task1.java
+++ src/main/java/a_introduction/Task1.java
@@ -4,8 +4,9 @@ public class Task1 {
     public static void main(String[] args) {
         int a = 9992;
         char b = 'K';
-        double c = 10.5;
-        long d = 665333;
+        double c = 10d;
+        long d = 665L;
         boolean e = 1 == 1;
+
     }
 }
--- src/main/java/a_introduction/Task2.java
+++ src/main/java/a_introduction/Task2.java
@@ -3,7 +3,7 @@ package a_introduction;
 public class Task2 {
     public static void main(String[] args) {
         int magic = 2147483647;
-        magic++;
-        System.out.println(magic);
+        int result = magic + 1;
+        System.out.println(result);
     }
 }
