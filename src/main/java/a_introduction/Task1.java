ab082ff162853239c8fcef5f74d032aea3ad74dd
Msiw1 <mateusz.siwicki@dpdgroup.com>
2023-04-20 17:56:32 +0200

    lekcja 2

--- src/main/java/a_introduction/Task1.java
+++ src/main/java/a_introduction/Task1.java
@@ -2,5 +2,10 @@ package a_introduction;
 
 public class Task1 {
     public static void main(String[] args) {
+        int a = 9992;
+        char b = 'K';
+        double c = 10.5;
+        long d = 665333;
+        boolean e = 1 == 1;
     }
 }
--- src/main/java/a_introduction/Task2.java
+++ src/main/java/a_introduction/Task2.java
@@ -2,5 +2,8 @@ package a_introduction;
 
 public class Task2 {
     public static void main(String[] args) {
+        int magic = 2147483647;
+        magic++;
+        System.out.println(magic);
     }
 }
